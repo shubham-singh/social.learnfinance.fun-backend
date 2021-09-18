d4c936c49d571f11385cfaea7a22f0f8074b6b9e
Shubham Singh <shubhamsng7@gmail.com>
2021-09-18 23:57:11 +0000

    feat: add images in post

--- models/post.model.js
+++ models/post.model.js
@@ -10,6 +10,14 @@ const postSchema = new Schema({
     type: String,
     required: [true, 'Post body is empty']
   },
+  img: {
+    src: {
+      type: String
+    },
+    cloudinary_id: {
+      type: String
+    }
+  },
   likes: [{ type: Schema.Types.ObjectId, ref: 'profile' }],
   replies: [{ type: Schema.Types.ObjectId, ref: 'reply' }]
 }, { timestamps: true });
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const upload = require('../middleware/multer');
 const { checkUser } = require('../middleware/auth.middleware.js');
 
 const { getAllPost, getPost, post, deletePost, likeUnlikePost } = require('../controllers/post.controller.js');
+
+const imgUpload = upload.single("image");
 
 router.param('username', async (req, res, next ,id) => {
   try {
@@ -18,7 +20,7 @@ router
 .get('/:username', getAllPost)
 .get('/:username/:postID', getPost)
 .use(checkUser)
-.post('/', post)
+.post('/', imgUpload, post)
 .post('/:postID/react', likeUnlikePost)
 .delete('/:postID', deletePost)
 
